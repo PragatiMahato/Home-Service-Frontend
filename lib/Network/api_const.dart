0540e0bc2b0d1c07e90a715add0a10155b019807
PragatiMahato <mahatopragati06@gmail.com>
2023-03-31 22:38:35 +0545

    Provider Created

--- lib/Network/api_const.dart
+++
@@ -1,3 +0,0 @@
-class ApiConst {
-  static const baseUrl = 'http://192.168.101.7:3000/';
-}
--- lib/model/feedback_model.dart
+++
@@ -1,7 +0,0 @@
-class FeedbackModel {
-  String userId;
-  String message;
-  String name;
-
-  FeedbackModel({required this.message, required this.userId,required this.name});
-}
